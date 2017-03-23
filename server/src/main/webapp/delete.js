64999ae7a3a48f18cf839da295764ee0bda0d86f
Arthur MARTELLO <arthur.martello@etudiant.univ-lille1.fr>
2017-03-23 15:42:12 +0100

    Delete Web des Oeuvres

---
+++ server/src/main/webapp/delete.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    
+    $("#delete-btn").click(function (event) {
+        
+        console.log(parseInt($("#deleteOno").val()));
+        
+        $.ajax({
+            url: "v1/oeuvre/"+parseInt($("#deleteOno").val()),
+            type: "DELETE",
+            dataType: "json",
+            contentType : 'application/json',
+            success: function(json) {
+                console.log("Deleted: " + json);
+                alert("Deleted");
+            },
+            error: function( xhr, status, errorThrown) {
+                alert("Erreur: DELETE");
+            },
+        });
+        
+    });
+    
+});
