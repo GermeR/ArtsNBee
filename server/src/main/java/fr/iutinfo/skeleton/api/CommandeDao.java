4af3450a1f8b58da8b1e50899fec5796dcccdaec
Robin Germe <robin.germe@etudiant.univ-lille1.fr>
2017-03-23 09:28:35 +0100

    Acces REST Commandes

---
+++ server/src/main/java/fr/iutinfo/skeleton/api/CommandeDao.java
@@ -0,0 +1,40 @@
+package fr.iutinfo.skeleton.api;
+
+import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
+import org.skife.jdbi.v2.tweak.BeanMapperFactory;
+
+import java.util.List;
+
+public interface CommandeDao {
+    @SqlUpdate("create table commande(ono integer, login varchar(20), paiement boolean, envoi boolean, reception boolean, remuneration boolean, prix numeric(6,2), frais numeric(4,2), primary key (ono, login), foreign key (ono) references Commande(ono), foreign key (login) references user(login));")
+    void createCommandeTable();
+
+    @SqlUpdate("insert into commande (login,ono,paiement,envoi,reception,remuneration,prix,frais) values (:login, :ono, :paiement, :envoi, :reception, :remuneration, :prix, :frais)")
+    @GetGeneratedKeys
+    int insert(@BindBean() Commande commande);
+
+    @SqlQuery("select * from commande where ono = :ono")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    Commande findByName(@Bind("ono") String ono);
+
+    @SqlQuery("select * from commande where search like :ono")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<Commande> search(@Bind("login") String login);
+
+    @SqlUpdate("drop table if exists commande")
+    void dropCommandeTable();
+
+    @SqlUpdate("delete from commande where ono = :ono")
+    void delete(@Bind("ono") int ono);
+
+    @SqlQuery("select * from commande order by ono")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<Commande> all();
+
+    @SqlQuery("select * from commande where ono = :ono")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    Commande findById(@Bind("ono") int ono);
+
+    void close();
+}
---
+++ server/src/main/java/fr/iutinfo/skeleton/api/CommandeResource.java
@@ -0,0 +1,79 @@
+package fr.iutinfo.skeleton.api;
+
+import static fr.iutinfo.skeleton.api.BDDFactory.getDbi;
+import static fr.iutinfo.skeleton.api.BDDFactory.tableExist;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.iutinfo.skeleton.common.dto.CommandeDto;
+
+@Path("/commande")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+
+public class CommandeResource {
+    final static Logger logger = LoggerFactory.getLogger(CommandeResource.class);
+    private static CommandeDao dao = getDbi().open(CommandeDao.class);
+
+    public CommandeResource() throws SQLException {
+        if (!tableExist("commande")) {
+            logger.debug("Crate table Commandes");
+            dao.createCommandeTable();
+            dao.insert(new Commande("Robert", 1, false, false, false, false, 12, 1));
+        }
+    }
+
+    @POST
+    public fr.iutinfo.skeleton.api.CommandeDto createCommande(fr.iutinfo.skeleton.api.CommandeDto dto) {
+    	Commande Commande = new Commande();
+    	Commande.initFromDto(dto);
+    	//int id = dao.insert(Commande);
+        //dto.setId(id);
+        return dto;
+    }
+
+    @GET
+    @Path("/{ono}")
+    public fr.iutinfo.skeleton.api.CommandeDto getCommande(@PathParam("ono") String ono) {
+        Commande Commande = dao.findByName(ono);
+        if (Commande == null) {
+            throw new WebApplicationException(404);
+        }
+        return Commande.convertToDto();
+    }
+
+    @GET
+    public List<fr.iutinfo.skeleton.api.CommandeDto> getAllCommandes(@QueryParam("q") String query) {
+        List<Commande> Commandes;
+        if (query == null) {
+        	Commandes = dao.all();
+        } else {
+            logger.debug("Search users with query: " + query);
+            Commandes = dao.search("%" + query + "%");
+        }
+        return Commandes.stream().map(Commande::convertToDto).collect(Collectors.toList());
+    }
+
+    @DELETE
+    @Path("/{ono}")
+    public void deleteCommande(@PathParam("id") int ono) {
+        dao.delete(ono);
+    }
+
+}
